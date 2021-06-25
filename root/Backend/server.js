ca4ae78a12effd61a2822472ba59eab04c3ef921
Ross <rossmorrison53@yahoo.com>
2021-06-25 11:18:30 -0700

    Created user and product models

--- root/Backend/server.js
+++ root/Backend/server.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT
 const db_url = process.env.DB_URL
 
+
 mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {console.log("connected to database!")})
 .catch((err) => console.log(err))

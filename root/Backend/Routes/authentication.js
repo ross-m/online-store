66dee27a27313eb59f363b0914cb938e737a6a6d
Ross <rossmorrison53@yahoo.com>
2021-06-26 11:22:01 -0700

    Still working on auth

--- root/Backend/Routes/authentication.js
+++ root/Backend/Routes/authentication.js
@@ -1,5 +1,4 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
 const jwt = require('jsonwebtoken')
 const User = require('../Models/user')
 const bcrypt = require('bcrypt')
--- root/Backend/server.js
+++ root/Backend/server.js
@@ -1,4 +1,5 @@
-const env = require('dotenv').config()
+require('dotenv').config()
+
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
@@ -6,13 +7,11 @@ const port = process.env.PORT
 const db_url = process.env.DB_URL
 const authRoute = require('./Routes/authentication')
 
-
 mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {console.log("connected to database!")})
-.catch((err) => console.log(err))
+    .then(() => {console.log("connected to database!")})
+    .catch((err) => console.log(err))
 
-app.use('/auth',authRoute)
-
+app.use('/auth', authRoute)
 
 app.listen(port, () => {
     console.log("user connected")

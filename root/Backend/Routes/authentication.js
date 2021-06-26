51293115c1c008b1b883ee1dc37b780c0dd1600b
Ross <rossmorrison53@yahoo.com>
2021-06-26 10:47:17 -0700

    Worked on authentication

--- root/Backend/Routes/authentication.js
+++ root/Backend/Routes/authentication.js
@@ -8,16 +8,69 @@ router.post('/login', async (req, res) => {
     const currentUser = User.findOne({username: req.body.email})
 
     if (currentUser) {
+
         bcrypt.compare(req.body.password, currentUser.password, function(err, res) {
+
             if (res) {
 
+                const Token = jwt.sign(
+                    {
+                    email: currentUser.email
+                    },
+                    process.env.AUTH_KEY,
+                    {
+                        expiresIn: '1h'
+                    }
+                )
+
+                return res.status(200).json({message: "Login succesful!", token: Token})
+
             }   else {
+
                     return res.status(401).json({error: err})
+
             }
         })
-    } else {
+
+    }   else {
+
+        return res.status(404).json({error: "Could not find user with that email."})
 
     }
 })
 
+router.post('/register', async (req, res) => {
+    const potentialConflict = await mongoose.findOne({email: req.body.email})
+
+    if (potentialConflict) {
+
+        return res.status(409).json({error: "This email is already in use!"})
+
+    }   else {
+
+        bcrypt.hash(req.body.password, 10, async (err, hash) => {
+
+            if (err) {
+
+                return res.status(500).json({error: "Error registering user..."})
+
+            }   else {
+
+                const newUser = new User({
+                    email: req.body.email,
+                    password: hash,
+                    isAdmin: false
+                })
+
+                await newUser.save()
+                    .then(() => {
+                        return res.status(200).json({message: "User has been registered!"})
+                    })
+                    .catch((err) => {
+                        return res.status(500).json({error: "Error registering user..."})
+                    })
+            }
+        })
+    }
+})
 module.exports = router
--- root/Backend/server.js
+++ root/Backend/server.js
@@ -4,11 +4,16 @@ const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT
 const db_url = process.env.DB_URL
+const authRoute = require('./Routes/authentication')
+
 
 
 mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {console.log("connected to database!")})
 .catch((err) => console.log(err))
+
+
+app.use(authRoute)
 
 
 app.listen(port, () => {

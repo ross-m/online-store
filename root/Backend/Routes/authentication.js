3f2159ca528e63a74d6d651a67fba52e494efe90
Ross <rossmorrison53@yahoo.com>
2021-06-29 13:51:10 -0700

    Added administrator IP

--- root/Backend/Routes/authentication.js
+++ root/Backend/Routes/authentication.js
@@ -38,8 +38,9 @@ router.post('/login', async (req, res, next) => {
         }
 
     }   catch(err) {
-            console.log(err)
-            return res.status(500).json({error: "Something went wrong"})
+
+            return res.status(500).json({error: "Internal error"})
+
     }
 })
 
@@ -81,15 +82,52 @@ router.post('/register', async (req, res, next) => {
         }
 
     }   catch(err) {
-            console.log(err)
-        }
+
+            return res.status(500).json({error: "Internal error"})
+
+    }
 })
 
 
 
-router.post('/admin', ipfilter(process.env.admins), (req, res, next) => {
-    console.log('hello')
-    return res.status(200).json({message: "we have entered the admin page"})
+router.post('/admin', ipfilter([process.env.admin], {mode: 'allow'}), async (req, res, next) => {
+    try {
+        const attemptedAdmin = await User.findOne({email: req.body.email})
+
+        if (attemptedAdmin) {
+    
+                if (attemptedAdmin.isAdmin) {
+    
+                    const Token = jwt.sign(
+                        {
+                            email: currentUser,
+                            isAdmin: currentUser.isAdmin
+                        }, 
+                        process.env.AUTH_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+    
+                    return res.status(200).json({message: "Admin succesfully logged in!", token: Token})
+    
+                }   else {
+    
+                        return res.status(404).json("This user does not have administrative privelidges")
+    
+                }
+    
+        }   else {
+    
+                return res.status(404).json({message: "Can't find any user with that email..."})
+    
+        }
+        
+    }   catch {
+
+            return res.status(500).json({error: "Internal error"})
+
+    }
 })
 
 
@@ -97,12 +135,12 @@ router.post('/admin', ipfilter(process.env.admins), (req, res, next) => {
 router.use((err, req, res, next) => {
 
     if (err instanceof IpDeniedError) {
-
-      return res.status(401).json({error: "You are not authorized to access this resource"})
+        
+        return res.status(401).json({error: "You are not authorized to access this resource"})
 
     } else {
-
-      return res.status(500).json({error: "Error logging in..."})
+        
+        return res.status(500).json({error: "Error logging in..."})
 
     }
 

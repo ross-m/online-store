ae8c237f67a77fdd4d1765e70ac12b674df99659
Ross <rossmorrison53@yahoo.com>
2021-06-29 13:53:15 -0700

    Fixed spacing inconsistencies

--- root/Backend/Routes/authentication.js
+++ root/Backend/Routes/authentication.js
@@ -30,11 +30,15 @@ router.post('/login', async (req, res, next) => {
                 return res.status(200).json({message: "User succesfully logged in!", token: Token})
 
             }   else {
+
                     return res.status(404).json({error: "Incorrect password"})
+                    
             }
 
         }   else {
+
                 return res.status(404).json({error: "Can't find any user with that email..."})
+
         }
 
     }   catch(err) {
@@ -122,7 +126,7 @@ router.post('/admin', ipfilter([process.env.admin], {mode: 'allow'}), async (req
                 return res.status(404).json({message: "Can't find any user with that email..."})
     
         }
-        
+
     }   catch {
 
             return res.status(500).json({error: "Internal error"})

65b9df18b8c5fb7cd97d9164008c14ba7fd4ce65
Ross <rossmorrison53@yahoo.com>
2021-07-28 11:38:12 -0700

    Basic retrieval for women's clothing

--- root/Backend/Routes/home.js
+++ root/Backend/Routes/home.js
@@ -26,7 +26,7 @@ router.get('/Mens', async (req, res, next) => {
     
     const hits = await Promotion.find({category: /Mens/})
 
-    if (hits) { 
+    if(hits.length) { 
 
         return res.status(200).json({message: "Retrieved mens!", content: hits})
 
@@ -44,7 +44,7 @@ router.get('/Mens/Tops', async (req, res, next) => {
     
     const hits = await Promotion.find({category: {$regex: 'Mens/Tops/'}})
 
-    if (hits) { 
+    if(hits.length) { 
 
         return res.status(200).json({message: "Retrieved mens tops!", content: hits})
 
@@ -62,7 +62,7 @@ router.get('/Mens/Tops/Shirts', async (req, res, next) => {
     
     const hits = await Promotion.find({category: 'Mens/Tops/Shirts'})
 
-    if (hits) { 
+    if(hits.length) { 
 
         return res.status(200).json({message: "Retrieved mens shirts!", content: hits})
 
@@ -79,8 +79,8 @@ router.get('/Mens/Tops/Shirts', async (req, res, next) => {
 router.get('/Mens/Tops/Hoodies', async (req, res, next) => {
     
     const hits = await Promotion.find({category: 'Mens/Tops/Hoodies'})
-    console.log(hits)
-    if (hits) { 
+    
+    if (hits.length) { 
 
         return res.status(200).json({message: "Retrieved mens hoodies!", content: hits})
 
@@ -91,6 +91,187 @@ router.get('/Mens/Tops/Hoodies', async (req, res, next) => {
     }
 
 })
+
+
+
+router.get('/Mens/Bottoms', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: {$regex: 'Mens/Bottoms/'}})
+
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved mens bottoms!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Mens/Bottoms/Shorts', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: 'Mens/Bottoms/Shorts'})
+
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved mens shorts!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Mens/Bottoms/Pants', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: 'Mens/Bottoms/Pants'})
+    
+    if (hits.length) { 
+
+        return res.status(200).json({message: "Retrieved mens pants!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Womens', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: /Womens/})
+
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved womens!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Womens/Tops', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: {$regex: 'Womens/Tops/'}})
+
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved womens tops!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Womens/Tops/Shirts', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: 'Womens/Tops/Shirts'})
+
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved womens shirts!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Womens/Tops/Sweaters', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: 'Womens/Tops/Sweaters'})
+    console.log(hits)
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved womens hoodies!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Womens/Bottoms', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: {$regex: 'Womens/Bottoms/'}})
+
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved womens bottoms!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Womens/Bottoms/Shorts', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: 'Womens/Bottoms/Shorts'})
+
+    if(hits.length) { 
+
+        return res.status(200).json({message: "Retrieved womens shorts!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
+
+
+router.get('/Womens/Bottoms/Pants', async (req, res, next) => {
+    
+    const hits = await Promotion.find({category: 'Womens/Bottoms/Pants'})
+    
+    if (hits.length) { 
+
+        return res.status(200).json({message: "Retrieved womens pants!", content: hits})
+
+    }   else {
+
+        return res.status(404).json({message: "Nothing here!"})
+        
+    }
+
+})
+
 
 
 module.exports = router

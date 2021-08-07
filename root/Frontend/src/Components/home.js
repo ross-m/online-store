fb3365cd388d1fa9f4e22a53f0c144c981cb8501
Ross <rossmorrison53@yahoo.com>
2021-08-07 10:13:30 -0700

    Worked on navbar. Must change font.

--- root/Frontend/src/Components/home.js
+++ root/Frontend/src/Components/home.js
@@ -1,16 +1,63 @@
 import React from "react"
-import {Navbar, Container, Nav} from "react-bootstrap"
+import {Navbar, NavDropdown, Container, Nav, Table, Button, Col} from "react-bootstrap"
 
 function Home() {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
+                <Col xs={1}>
+                    <Navbar.Brand href="#home">E-Shop</Navbar.Brand>
+                </Col>
+                <Col sm={10}>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Mens" id="basic-nav-dropdown">
+                            <Table striped border hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <th><Nav.Link href="#Mens/Tops">Tops</Nav.Link></th>
+                                        <th><Nav.Link href="#Mens/Bottoms">Bottoms</Nav.Link></th>
+                                    </tr>
+                                </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><Nav.Link href="#Mens/Shirts">Shirts</Nav.Link></td>
+                                            <td><Nav.Link href="#Mens/Shorts">Shorts</Nav.Link></td>
+                                        </tr>
+                                        <tr>
+                                            <td><Nav.Link href="#Mens/Hoodies">Hoodies</Nav.Link></td>
+                                            <td><Nav.Link href="#Mens/Pants">Pants</Nav.Link></td>
+                                        </tr>
+                                    </tbody>
+                            </Table>
+                        </NavDropdown>
+                        <NavDropdown title="Womens" id="basic-nav-dropdown">
+                            <Table striped border hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <th><Nav.Link href="#Womens/Tops">Tops</Nav.Link></th>
+                                        <th><Nav.Link href="#Womens/Bottoms">Bottoms</Nav.Link></th>
+                                    </tr>
+                                </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><Nav.Link href="#Womens/Shirts">Shirts</Nav.Link></td>
+                                            <td><Nav.Link href="#Womens/Shorts">Shorts</Nav.Link></td>
+                                        </tr>
+                                        <tr>
+                                            <td><Nav.Link href="#Womens/Hoodies">Sweaters</Nav.Link></td>
+                                            <td><Nav.Link href="#Womens/Pants">Pants</Nav.Link></td>
+                                        </tr>
+                                    </tbody>
+                            </Table>
+                        </NavDropdown>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Col>
+                <Col>
+                    <Button variant="primary">Primary</Button>{' '}
+                </Col>
             </Container>
         </Navbar>
     )
